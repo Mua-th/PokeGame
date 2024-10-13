51ee23f3b0edc52afc95c9ad477471e17018e9ed
mua-th <mouadelgharbi2002@gmail.com>
2024-10-13 19:32:32 +0200

    adding inquirer & fetching pokemons list

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
+import getPokemons from  './pokemons.js';
+import inquirer from 'inquirer';
+
+const   pokemons =  async () => await getPokemons()
+
+
+
 console.log("------------------ POKEMON GAME ------------------");
+
+inquirer
+  .prompt([
+        {
+            name: "pokemon",
+            type: "list",
+            message: "choose your pokemon ",
+            choices : pokemons
+        }
+  ])
+  .then((answers) => {
+    console.log(answers.user_name)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
---
+++ pokemons.js
@@ -0,0 +1,23 @@
+
+const result = []
+ const getPokemons = async ()=> { 
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+    
+        
+        const json = await response.json();
+        json.results.forEach(element => {
+            result.push(element.name)          
+        });
+        //console.log(result);
+        return result;
+      } catch (error) {
+        console.error(error.message);
+      }}
+
+     //console.log(getPokemons())
+export default getPokemons; 
+  
